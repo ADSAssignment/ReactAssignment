e279a8fcea20e5761144efc72b86eb59cc684dde
sanjeevsharmaads <sanjeev.sharma@affluencesolutions.com>
2019-01-23 09:49:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navigation from './Navigation';
-import './App.css';
+import './ResponsiveNavigation.css';
 
 class App extends Component {
   render() {
