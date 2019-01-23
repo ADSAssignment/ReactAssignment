5207cbabe556328e5aa3fdaa49ac960d51709385
Tarsem Jangra <tarsemk.jangra@gmail.com>
2019-01-23 17:58:07 +0530

    Select Country, State and City

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Select.css';
+import Assignment1 from './Assignment1';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Assignment1 />
+      </div>
+    );
+  }
+}
+
+export default App;
